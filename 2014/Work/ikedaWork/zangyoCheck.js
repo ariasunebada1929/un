27dd48a0946a36fb09837a531617bfd16e830072
IKEDAIKEDA <ikeda1990ikeda@gmail.com>
2014-05-27 00:34:13 +0900

    池田　残業調整js変更

--- 2014/Work/ikedaWork/zangyoCheck.js
+++ 2014/Work/ikedaWork/zangyoCheck.js
@@ -1,18 +1,20 @@
 function zangyou_Change(){
+	for(n = 1; n <=31; n++){
 		obj = document.getElementById("Zangyo_chousei");
 		index= obj.selectedIndex;
 		 // IDをキーにしてテキストボックスエレメントの参照を取得する
-        var targetElement = document.getElementById('id_zangyou');
+        var targetElement = document.getElementById('id_zangyou' + n);
 		if(index != 0){
         // テキストボックスを入力不可にします
         targetElement.readOnly = false;
 		targetElement.style.backgroundColor='#FFFFFF';
-        alert("入力可になりました。");
+        // alert("入力可になりました。");
 		}
 		else{
         // テキストボックスを入力可にします
         targetElement.readOnly = true;
 		targetElement.style.backgroundColor='#808080';
-        alert("入力不可になりました。");
+        //alert("入力不可になりました。");
         }
+     }
 }
