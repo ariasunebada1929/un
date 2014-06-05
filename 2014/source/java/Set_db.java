98a360fb3fda7f24474261dd7b08a5de8e0134d6
ariasunebada1929 <ariasu1111@yahoo.co.jp>
2014-06-05 21:50:55 +0900

    2014/06/05 わたるん Insert_テスト成功

---
+++ 2014/source/java/Set_db.java
@@ -0,0 +1,118 @@
+package info.un_test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class Set_db extends HttpServlet{
+	public String str_message;
+	public String true_time;
+
+	//メイン処理(戻り値：結果メッセージ　受取値：登録初回フラグ)
+	public ArrayList<String> main_action(String flg) throws SQLException{
+
+		//メッセージ格納配列
+		ArrayList<String> message = new ArrayList<String>();
+
+		//DB接続用変数定義
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		//DB接続情報を設定する
+		String path = "jdbc:mysql://localhost/unserver2014";  //接続パス
+		String id = "root";    //ログインID
+		String pw = "";  //ログインパスワード
+
+		//SQL実行命令
+		//初回インサート命令
+		/*		String sql_in = "INSERT INTO unserver_01.WORK_TRN 	(STAFF_ID,YEAR,MONTH,DAY,"
+				+ "SUBMIT_REQUEST_1_CD,SUBMIT_REQUEST_2_CD,"
+				+ "SUBMIT_REQUEST_3_CD,SUBMIT_REQUEST_4_CD,"
+				+ "COMP_HOLIDAY,DETAIL,BASIC_WORK_START,BASIC_WORK_END,"
+				+ "ACTUAL_WORK_START,ACTUAL_WORK_END,RESTHOURS,ZANGYO_ADJUST) "
+				+ "VALUES ('00001','2014','05','06',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ')";
+		 */
+		String sql_in = "INSERT INTO unserver2014.`parsonal_auth_trn`(STAFF_ID, PASSWORD) VALUES ( '00002', 'uehara')";
+
+		//編集アップロード命令
+		String sql_up = "SELECT * FROM unserver2014.`parsonal_auth_trn`";
+
+		try{
+			//JDBCドライバをロードする
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("接続成功");
+			//DBへのコネクションを作成する
+			conn = DriverManager.getConnection(path, id, pw);
+			Statement stmt = conn.createStatement();
+			//conn.setAutoCommit(false);//オートコミットオフ
+
+			//処理
+			if (flg == "1"){
+				//当月分の日時取得処理
+				Calendar cal = Calendar.getInstance();
+				int year  = cal.get(Calendar.YEAR);
+				int month = cal.get(Calendar.MONTH) + 1;
+				//月末取得
+				cal.set(Calendar.YEAR, year);
+				cal.set(Calendar.MONTH, month );                
+				int lastDayOfMonth = cal.getActualMaximum(Calendar.DATE);
+
+				//HTMLからデータ取得
+				//for(int i=1; i<=lastDayOfMonth; i++){
+				for(int i=1; i<=3; i++){	
+
+					//文字列チェック
+					System.out.println(Set_db.check("900"));
+
+					//一カ月分のインサート
+					ps = conn.prepareStatement(sql_in);
+				}
+			} else if(flg == "2"){
+				//アップロード
+
+			}
+			
+			stmt.execute(sql_in);
+			//conn.commit();//コミット
+
+		}catch(Exception ex){
+			//例外発生時の処理
+			//conn.rollback();//ロールバック
+			ex.printStackTrace();  //エラー内容をコンソールに出力する
+		}finally{
+			//クローズ処理
+			if(rs != null) rs.close();
+			if(ps != null) ps.close();
+			if(conn != null) conn.close();
+		}
+		//引数：結果メッセージ
+		return message;	
+	}
+
+	//入力時間チェック
+	public static String check(String check_time){
+		String str = check_time;
+		String zero = "0";
+		String time = null;
+
+		//桁を求める
+		int len = str.length();
+
+		//三桁の場合は頭0を付ける
+		if(len == 3){
+			time = zero + check_time;
+		}
+		return time;
+	}
+}
