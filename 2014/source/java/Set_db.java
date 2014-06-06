a125119b279bf5e57983cb189d753a28c89009ac
ariasunebada1929 <ariasu1111@yahoo.co.jp>
2014-06-06 22:16:11 +0900

    2014/06/06 わたるん Insertのパラメータ部を作成 日本語の文字化け対応が未解決

--- 2014/source/java/Set_db.java
+++ 2014/source/java/Set_db.java
@@ -13,13 +13,14 @@ import java.util.Calendar;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 public class Set_db extends HttpServlet{
 	public String str_message;
 	public String true_time;
 
 	//メイン処理(戻り値：結果メッセージ　受取値：登録初回フラグ)
-	public ArrayList<String> main_action(String flg) throws SQLException{
+	public ArrayList<String> main_action(HttpServletRequest req , String flg) throws SQLException{
 
 		//メッセージ格納配列
 		ArrayList<String> message = new ArrayList<String>();
@@ -43,8 +44,6 @@ public class Set_db extends HttpServlet{
 				+ "ACTUAL_WORK_START,ACTUAL_WORK_END,RESTHOURS,ZANGYO_ADJUST) "
 				+ "VALUES ('00001','2014','05','06',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ')";
 		 */
-		String sql_in = "INSERT INTO unserver2014.`parsonal_auth_trn`(STAFF_ID, PASSWORD) VALUES ( '00002', 'uehara')";
-
 		//編集アップロード命令
 		String sql_up = "SELECT * FROM unserver2014.`parsonal_auth_trn`";
 
@@ -58,7 +57,7 @@ public class Set_db extends HttpServlet{
 			//conn.setAutoCommit(false);//オートコミットオフ
 
 			//処理
-			if (flg == "1"){
+			if (flg.equals("1")){
 				//当月分の日時取得処理
 				Calendar cal = Calendar.getInstance();
 				int year  = cal.get(Calendar.YEAR);
@@ -70,20 +69,46 @@ public class Set_db extends HttpServlet{
 
 				//HTMLからデータ取得
 				//for(int i=1; i<=lastDayOfMonth; i++){
-				for(int i=1; i<=3; i++){	
-
+				String sql_in = "";
+				
+				for(int i=1; i<=lastDayOfMonth - 1; i++){
+					sql_in = sql_in 
+							+ " INTO unserver2014.WORK_TRN "  
+							+ "(STAFF_ID,YEAR,MONTH,DAY,"
+				            + "SUBMIT_REQUEST_1_CD,SUBMIT_REQUEST_2_CD,"
+				            + "SUBMIT_REQUEST_3_CD,SUBMIT_REQUEST_4_CD,"
+				            + "COMP_HOLIDAY,DETAIL,BASIC_WORK_START,BASIC_WORK_END,"
+				            + "ACTUAL_WORK_START,ACTUAL_WORK_END,RESTHOURS,ZANGYO_ADJUST) VALUES ( " 
+				            + "'0002','"
+				            + year + "','" 
+				            + String.format("%1$02d", month) + "','" 
+				            + String.format("%1$02d", i) + "','"
+				            + String.format("%1$02d",Integer.valueOf(req.getParameter("nm_Kyuka_" + i))) + "','"
+			                + String.format("%1$02d",Integer.valueOf(req.getParameter("nm_KyusyutuFuridai_" + i))) + "','"
+			                + String.format("%1$02d",Integer.valueOf(req.getParameter("nm_Shift_" + i))) + "',"
+			                + String.valueOf(req.getParameter("nm_furikae_" + i)) + "','"          
+			                + String.valueOf(req.getParameter("nm_syousai_" + i)) + "','"
+			                + String.format("%1$02d",Integer.valueOf(req.getParameter("nm_kihonS_" + i))) + "','"
+			                + String.format("%1$02d",Integer.valueOf(req.getParameter("nm_kihonE_" + i))) + "','"
+			                + String.format("%1$02d",Integer.valueOf(req.getParameter("nm_jissekiS_" + i))) + "','"
+			                + String.format("%1$02d",Integer.valueOf(req.getParameter("nm_jissekiE_" + i))) + "','"
+			                + String.format("%1$02d",Integer.valueOf(req.getParameter("nm_jiseekiR_" + i))) + "','"
+			                + String.format("%1$02d",Integer.valueOf(req.getParameter("nm_zangyou_" + i))) + "');";
+			                
 					//文字列チェック
-					System.out.println(Set_db.check("900"));
+					System.out.println(i);
 
-					//一カ月分のインサート
-					ps = conn.prepareStatement(sql_in);
 				}
+				
+				sql_in = new String(sql_in.getBytes("Shift_JIS"),"Shift_JIS");
+				
+				stmt.execute(sql_in);
+				
 			} else if(flg == "2"){
 				//アップロード
 
 			}
 			
-			stmt.execute(sql_in);
 			//conn.commit();//コミット
 
 		}catch(Exception ex){
